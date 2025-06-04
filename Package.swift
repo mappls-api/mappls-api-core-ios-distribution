c5a836cffd0e80c1dfbbf34ad1a448350d6803e6
siddharth <siddharth.k@mapmyindia.com>
2025-06-04 15:48:34 +0530

    - Initial release version 2.0.0

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MapplsAPICore",
+    products: [
+        .library(
+            name: "MapplsAPICore",
+            targets: ["MapplsAPICore"])
+    ],
+    dependencies: [
+    ],    
+    targets: [
+        .binaryTarget(
+            name: "MapplsAPICore",
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/MapplsAPICore-2.0.0.zip",
+            checksum: "22434cad01f10ce6680328a257a9928a18905ff52f4028dc4c6827ffe44c3626")
+    ]
+)
