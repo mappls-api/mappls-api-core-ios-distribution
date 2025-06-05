5742ec7bccdcea8973aeed1b8f0441544979ec79
siddharth <siddharth.k@mapmyindia.com>
2025-06-05 19:20:57 +0530

    released : version : 2.0.4
    - fixed checksum issue

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapplsAPICore",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/MapplsAPICore-2.0.3.zip",
-            checksum: "21278a279d2642cd6bf22ecb4568a49a4829618f196d739268e4b1edf8772ecb")
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/MapplsAPICore-2.0.4.zip",
+            checksum: "e3b75c9e8f861b42fe6c6ecff4ff095d3279a006d07940ed2c8ce006c61ba2aa")
     ]
 )
