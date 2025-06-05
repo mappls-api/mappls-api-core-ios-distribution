2b3e9ff31583300af5ae5df5ea72978a056c59a9
siddharth <siddharth.k@mapmyindia.com>
2025-06-05 18:59:33 +0530

    Released - version - 2.0.2
    Change log:
    - Improvements and bug fixes

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapplsAPICore",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/MapplsAPICore-2.0.1.zip",
-            checksum: "4e82c61a57f1c38f7168903c818822a9b5ee48a98b3bca59600da4a3958aaac1")
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/MapplsAPICore-2.0.2.zip",
+            checksum: "21278a279d2642cd6bf22ecb4568a49a4829618f196d739268e4b1edf8772ecb")
     ]
 )
