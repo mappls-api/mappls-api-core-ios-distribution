a6ab340725677bbaa4ca12b489a2e2e2a849c05a
siddharth <siddharth.k@mapmyindia.com>
2025-10-14 18:31:54 +0530

    Fixed payload issue where team id is mandatory.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapplsAPICore",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/MapplsAPICore-2.0.5.zip",
-            checksum: "713be4743ffd4cf728cf86516f7a27ad3a5dc6374d61944dd53ece72f962fa0f")
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/MapplsAPICore-2.0.6.zip",
+            checksum: "1ac0c35dc53d40f3589bfa2cfd4463ace4233b094215a5067fdcba8c939ac3c3")
     ]
 )
